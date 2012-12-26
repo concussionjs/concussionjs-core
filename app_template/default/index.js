e35741d4612e9b8eb037c42a551df5c55834ebb4
root <ben@concussionjs.com>
2012-12-26 16:25:38 -0500

    modified the default template to use connect to serve static files

--- app_template/default/index.js
+++ app_template/default/index.js
@@ -1,22 +1,15 @@
 var http = require("http")
+var connect = require("connect")
 var nta = require("./node_modules/nextera/nextera.js")
 var settings = require("./settings.js");
 var url;
 
 var s = settings();
 
-function writeRes(res)
-{
-	 res.end('application ' + s.name + ' has started\n');
-}
-
-var server = nta.createServerWithStaticFiles(
-
-	s,function (req, res) 
-	{
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-  		writeRes(res);  
-	}
+var server = connect.createServer(
+	connect.logger(), // Log responses to the terminal using Common Log Format.
+	connect.compress(), // Gzip the output stream when the browser wants it.
+	connect.static(__dirname) // Serve all static files in the current dir.
 );
 
-//nta.getPort(server,s.id);
+nta.listen(server, s.id);

fd75207e93fc861ded31d298ad4c648404257681
root <ben@concussionjs.com>
2013-04-07 20:14:03 -0400

    replaced reference to stringify plugin with JSON equivalent

--- apps/api/js/cjs-bootstrap.js
+++ apps/api/js/cjs-bootstrap.js
@@ -9,7 +9,7 @@ window.onload = function(){
 				var error;
 				//console.log($.stringify(cjs.inferObjects(page.html)));
 				//console.log("countme: " + (countme++));
-				$.post("http://api.local-concussionjs.com/postGetScript/true/?sid="+sessionId,$.stringify(cjs.inferObjects(page.html)),function(result){
+				$.post("http://api.local-concussionjs.com/postGetScript/true/?sid="+sessionId,JSON.stringify(cjs.inferObjects(page.html)),function(result){
 						//console.log(result);
 						eval(result);							
 						$("body").append("<a href=\"#\" onclick=\"window.open(\'http://api.local-concussionjs.com/admin.html?id=" + sessionId + "\');\"> <img style=\"position: absolute; top: 0; right: 0; border: 0;\" src=\"http://api.local-concussionjs.com/concussion_admin.png\" alt=\"Fork me on GitHub\"> </a>");

824c92555e4cfde82c09ce734807d5d51ea78fa5
concussed <concussed@coreubuntu.(none)>
2013-08-24 05:01:35 -0400

    updated to remove dependencies on /home/concussed environment variable

--- install/concussion.sh
+++ install/concussion.sh
@@ -1,5 +1,5 @@
-export CJS_APPS=$HOME/concussionjs-core/apps
-export CJS_APPS_TEMPLATES=$HOME/concussionjs-core/app_template
+export CJS_APPS=@HOME@/apps
+export CJS_APPS_TEMPLATES=@HOME@/app_template
 export CJS_WEB_URL=api.local-concussionjs.com
 export CJS_WEB_DOMAIN=local-concussionjs.com
 
--- install/install.sh
+++ install/install.sh
@@ -10,7 +10,7 @@ function usage
 
 function set_root_dir_global
 {
-	root_dir=$(npm root -g)
+	root_dir=$(npm root -g)/concussionjs-core
 }
 
 function set_root_dir_local
@@ -44,46 +44,45 @@ case $1 in
 esac
 pip install pymongo
 pip install redis
-make --directory $root_dir/concussionjs-core/install/mon install
-#echo "starting concussionjs-core npm install"
-#npm --prefix $HOME/concussionjs-core install $HOME/concussionjs-core
+make --directory $root_dir/install/mon install
+echo "starting concussionjs-core npm install"
 #Make cjs executable accessible globally
-chown -R concussed:concussed $root_dir
-#ln -s /usr/local/lib/node_modules $HOME/concussionjs-core/proxy/node_modules
-#install upstart scripts
-rm -f $root_dir/concussionjs-core/node_modules/concussionjs-core
-ln -s $root_dir/concussionjs-core $root_dir/concussionjs-core/node_modules/concussionjs-core
+chown -R concussed:concussed $root_dir/..
 
-cp -f $root_dir/concussionjs-core/install/upstart_scripts/api.conf /etc/init
-cp -f $root_dir/concussionjs-core/install/upstart_scripts/samples.conf /etc/init
-cp -f $root_dir/concussionjs-core/install/upstart_scripts/redis-server.conf /etc/init
+#install upstart scripts
+rm -f $root_dir/node_modules/concussionjs-core
+ln -s $root_dir $root_dir/node_modules/concussionjs-core
+
+cp -f $root_dir/install/upstart_scripts/api.conf /etc/init
+cp -f $root_dir/install/upstart_scripts/samples.conf /etc/init
+cp -f $root_dir/install/upstart_scripts/redis-server.conf /etc/init
 
 if [ "$1" = "-g" ]; then
-	cp -f $root_dir/concussionjs-core/install/concussion_global.sh /etc/profile.d/concussion.sh	
-	cp -f $root_dir/concussionjs-core/install/upstart_scripts/cjs-proxy-global.conf /etc/init/cjs-proxy.conf
+	cp -f $root_dir/install/concussion_global.sh /etc/profile.d/concussion.sh	
+	cp -f $root_dir/install/upstart_scripts/cjs-proxy-global.conf /etc/init/cjs-proxy.conf
 fi
 
 if [ "$1" = "" ]; then
-	cp -f $root_dir/concussionjs-core/install/concussion.sh /etc/profile.d	
-	sed -e "s;@HOME@;$root_dir;" $root_dir/concussionjs-core/install/upstart_scripts/cjs-proxy.conf > /etc/init/cjs-proxy.conf
+	sed -e "s;@HOME@;$root_dir;" $root_dir/install/concussion.sh > /etc/profile.d/concussion.sh
+	sed -e "s;@HOME@;$root_dir;" $root_dir/install/upstart_scripts/cjs-proxy.conf > /etc/init/cjs-proxy.conf
 fi
 
 if [ "$1" = "-d" ]; then
-	cp -f $root_dir/concussionjs-core/install/concussion.sh /etc/profile.d
-        ln -s $root_dir/concussionjs-core/bin/cli.py /usr/local/bin/cjs
-        ln -s $root_dir/concussionjs-core/node_modules/concussionjs-proxy/bin/cjs-proxy /usr/local/bin/cjs-proxy
-        sed -e "s;@HOME@;$root_dir;" $root_dir/concussionjs-core/install/upstart_scripts/cjs-proxy.conf > /etc/init/cjs-proxy.conf
+	sed -e "s;@HOME@;$root_dir;" $root_dir/install/concussion.sh > /etc/profile.d/concussion.sh
+        ln -s $root_dir/bin/cli.py /usr/local/bin/cjs
+        ln -s $root_dir/node_modules/concussionjs-proxy/bin/cjs-proxy /usr/local/bin/cjs-proxy
+        sed -e "s;@HOME@;$root_dir;" $root_dir/install/upstart_scripts/cjs-proxy.conf > /etc/init/cjs-proxy.conf
 fi
 
 chmod +x /etc/profile.d/concussion.sh
 su - concussed -c /etc/profile.d/concussion.sh
 /etc/init.d/redis-server stop
-cp -f $root_dir/concussionjs-core/install/dump.rdb /var/lib/redis/dump.rdb
+cp -f $root_dir/install/dump.rdb /var/lib/redis/dump.rdb
 /etc/init.d/redis-server start
 #mongo imports
-mongoimport --db concussion_prod --collection apps --file $root_dir/concussionjs-core/install/mongodb_initialize/apps.json --type json
-mongoimport --db concussion_prod --collection proxies --file $root_dir/concussionjs-core/install/mongodb_initialize/proxies.json --type json
-mongoimport --db concussion_prod --collection cjs_objects --file $root_dir/concussionjs-core/install/mongodb_initialize/cjs_objects.json --type json
+mongoimport --db concussion_prod --collection apps --file $root_dir/install/mongodb_initialize/apps.json --type json
+mongoimport --db concussion_prod --collection proxies --file $root_dir/install/mongodb_initialize/proxies.json --type json
+mongoimport --db concussion_prod --collection cjs_objects --file $root_dir/install/mongodb_initialize/cjs_objects.json --type json
 #starting services
 service redis-server start
 service cjs-proxy start

0d24679267a615b9f2a891ffb3360eaa27699a1c
concussed <concussed@coreubuntu.(none)>
2013-09-09 19:36:57 -0400

    updated install script to take arguments

--- install/install.sh
+++ install/install.sh
@@ -1,27 +1,56 @@
-make --directory $(npm root -g)/concussionjs-core/install/mon install
+root_dir=
+
+function set_root_dir_global
+{
+	root_dir=$(npm root -g)
+}
+
+function set_root_dir_local
+{
+	root_dir=$(npm root)
+}
+
+if [ "$1" = "" ]; then
+	set_root_dir_local
+fi
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -g | --global )			set_root_dir_global
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;    
+        *)						usage
+								exit 1
+    esac
+    shift
+done
+
+make --directory $(root_dir)/concussionjs-core/install/mon install
 #echo "starting concussionjs-core npm install"
 #npm --prefix $HOME/concussionjs-core install $HOME/concussionjs-core
 #Make cjs executable accessible globally
-chown -R concussed:concussed $(npm root -g)
+chown -R concussed:concussed $(root_dir)
 #ln -s /usr/local/lib/node_modules $HOME/concussionjs-core/proxy/node_modules
 #install upstart scripts
-rm -f $(npm root -g)/concussionjs-core/node_modules/concussionjs-core
-ln -s $(npm root -g)/concussionjs-core $(npm root -g)/concussionjs-core/node_modules/concussionjs-core
+rm -f $(root_dir)/concussionjs-core/node_modules/concussionjs-core
+ln -s $(root_dir)/concussionjs-core $(root_dir)/concussionjs-core/node_modules/concussionjs-core
 
-cp -f $(npm root -g)/concussionjs-core/install/upstart_scripts/cjs-proxy.conf /etc/init
-cp -f $(npm root -g)/concussionjs-core/install/upstart_scripts/api.conf /etc/init
-cp -f $(npm root -g)/concussionjs-core/install/upstart_scripts/samples.conf /etc/init
-cp -f $(npm root -g)/concussionjs-core/install/upstart_scripts/redis-server.conf /etc/init
-cp -f $(npm root -g)/concussionjs-core/install/concussion.sh /etc/profile.d
+cp -f $(root_dir)/concussionjs-core/install/upstart_scripts/cjs-proxy.conf /etc/init
+cp -f $(root_dir)/concussionjs-core/install/upstart_scripts/api.conf /etc/init
+cp -f $(root_dir)/concussionjs-core/install/upstart_scripts/samples.conf /etc/init
+cp -f $(root_dir)/concussionjs-core/install/upstart_scripts/redis-server.conf /etc/init
+cp -f $(root_dir)/concussionjs-core/install/concussion.sh /etc/profile.d
 chmod +x /etc/profile.d/concussion.sh
 su - concussed -c /etc/profile.d/concussion.sh
 /etc/init.d/redis-server stop
-cp -f $(npm root -g)/concussionjs-core/install/dump.rdb /var/lib/redis/dump.rdb
+cp -f $(root_dir)/concussionjs-core/install/dump.rdb /var/lib/redis/dump.rdb
 /etc/init.d/redis-server start
 #mongo imports
-mongoimport --db concussion_prod --collection apps --file $(npm root -g)/concussionjs-core/install/mongodb_initialize/apps.json --type json
-mongoimport --db concussion_prod --collection proxies --file $(npm root -g)/concussionjs-core/install/mongodb_initialize/proxies.json --type json
-mongoimport --db concussion_prod --collection cjs_objects --file $(npm root -g)/concussionjs-core/install/mongodb_initialize/cjs_objects.json --type json
+mongoimport --db concussion_prod --collection apps --file $(root_dir)/concussionjs-core/install/mongodb_initialize/apps.json --type json
+mongoimport --db concussion_prod --collection proxies --file $(root_dir)/concussionjs-core/install/mongodb_initialize/proxies.json --type json
+mongoimport --db concussion_prod --collection cjs_objects --file $(root_dir)/concussionjs-core/install/mongodb_initialize/cjs_objects.json --type json
 #starting services
 service redis-server restart
 service cjs-proxy start

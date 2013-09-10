4f458ac9fd86df158e5449ba60b9c8a703c93f39
concussed <concussed@coreubuntu.(none)>
2013-09-09 20:09:00 -0400

    added support for global and local installs

---
+++ install/concussion_global.sh
@@ -0,0 +1,5 @@
+export CJS_APPS=$(npm root -g)/concussionjs-core/apps
+export CJS_APPS_TEMPLATES=$(npm root -g)/concussionjs-core/app_template
+export CJS_WEB_URL=api.local-concussionjs.com
+export CJS_WEB_DOMAIN=local-concussionjs.com
+
